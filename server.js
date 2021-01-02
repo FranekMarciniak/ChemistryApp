0a537f88e756db4f1136a31cf7be72413fb6e866
franciszek <prostyfranek@protonmail.com>
2021-01-02 01:11:10 +0100

    yello

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/test", require("./routes/test"));
 app.use("/api/users", require("./routes/users"));
 
 const PORT = process.env.port || 6000;
-
+console.log(PORT);
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
