34baaf453e06256d1700272b5cc0af4d0b0a11e2
franciszek <prostyfranek@protonmail.com>
2021-01-02 22:04:01 +0100

    Added showing correct answer.

--- client/src/components/ExerciseComponents/ExerciseTestForm.js
+++ client/src/components/ExerciseComponents/ExerciseTestForm.js
@@ -4,9 +4,13 @@ import MiddleArrow from "../UniversalComponents/MiddleArrow";
 import { ExerciseCreatorContext } from "../../context/exerciseCreator/exerciseCreatorState";
 import RightSide from "./RightSide";
 import LeftSide from "./LeftSide";
-
-import { Button, Typography } from "@material-ui/core";
+import { Button, Typography, Dialog } from "@material-ui/core";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import ExerciseName from "./ExerciseName";
+import SmallMiddleArrow from "../../components/UniversalComponents/SmallMiddleArrow";
 const ExerciseFormWrapper = styled.div`
   width: 90%;
   height: 100px;
@@ -26,6 +30,15 @@ const ButtonWrapper = styled.div`
 `;
 
 function ExerciseCreatorForm() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   //Using useContext hook to bring in global state
   const exerciseCreatorContext = useContext(ExerciseCreatorContext);
   //Destructuring methods from global context
@@ -35,6 +48,7 @@ function ExerciseCreatorForm() {
     setExerciseFromBlueprint,
     getTestExerciseFromAPI,
     postTestExerciseToAPI,
+    testExercise,
   } = exerciseCreatorContext;
 
   useEffect(() => {
@@ -63,15 +77,62 @@ function ExerciseCreatorForm() {
         </Button>
         {currentExercise.leftSide === undefined ||
         currentExercise.leftSide.length > 0 ? (
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => postTestExerciseToAPI(currentExercise)}
-          >
-            Check!
-          </Button>
+          <>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleClickOpen}
+            >
+              Show me the answer
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => postTestExerciseToAPI(currentExercise)}
+            >
+              Check!
+            </Button>
+          </>
         ) : null}
       </ButtonWrapper>
+      {testExercise.leftSide !== undefined &&
+      testExercise.rightSide !== undefined &&
+      testExercise.rightSide.length > 0 &&
+      testExercise.leftSide.length > 0 ? (
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{testExercise.name}</DialogTitle>
+          <DialogContent
+            style={{
+              width: "300px",
+              display: "flex",
+            }}
+          >
+            <DialogContentText id="alert-dialog-description">
+              {testExercise.leftSide.map((blob, index) => {
+                let last = " + ";
+                if (index >= testExercise.leftSide.length - 1) {
+                  last = "";
+                }
+                return blob.ratio + " " + blob.value + last;
+              })}
+            </DialogContentText>
+            <SmallMiddleArrow text={testExercise.top} />
+            <DialogContentText id="alert-dialog-description">
+              {testExercise.rightSide.map((blob, index) => {
+                let last = " + ";
+                if (index >= testExercise.rightSide.length - 1) {
+                  last = "";
+                }
+                return blob.ratio + " " + blob.value + last;
+              })}
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+      ) : null}
     </ExerciseFormWrapper>
   );
 }
